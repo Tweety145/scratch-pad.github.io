471565c718badf21785e74387a44e105439c41a3
Tweety145 <teeandchris12@gmail.com>
2025-10-22 09:38:10 -0500

    describe work

--- day-1/length.js
+++ day-1/length.js
@@ -1,48 +1,47 @@
 // #!/usr/bin/env node
-
-'use strict'
+'use strict';
 /**
  * IN CLASS EXERCISE: stringy.js
  */
-
 /**
  * Given an input String, return its length.
  *
  * TIP: In JavaScript, how can we decipher the length of a String?
  * work?
  */
-
 /*
 I: Function takes in an input string.
 O: Function returns the length of the input string.
 C:
 E:
 */
-
 function length(string) {
-    // YOUR CODE BELOW HERE //
-  
-    // YOUR CODE ABOVE HERE //
-  }
-  
-  /**
-   * Given an input String, return a new String forced to lowercase.
-   */
+  // YOUR CODE BELOW HERE //
+  //return length of input string
+  return string.length;
+
+  // YOUR CODE ABOVE HERE /
+  /*
+  I: Function give an string
+  O: Function return a string with uppercase
+  C:
+  E:
+  */
   function toLowerCase(string) {
     // YOUR CODE BELOW HERE //
-  
+    //return a new uppercase string
+    return;
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given an input String, return a new String forced to uppercase.
    */
   function toUpperCase(string) {
     // YOUR CODE BELOW HERE //
-  
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given an input String, return a new String forced to dash-case.
    *
@@ -58,10 +57,9 @@ function length(string) {
    */
   function toDashCase(string) {
     // YOUR CODE BELOW HERE //
-  
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given an input String and a single character, return true if the String
    * begins with the character, false otherwise. The Function is case insensitive.
@@ -76,10 +74,9 @@ function length(string) {
    */
   function beginsWith(string, char) {
     // YOUR CODE BELOW HERE //
-  
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given an input String and a single character, return true if the String
    * ends with the character, false otherwise. The Function is case insensitive.
@@ -94,10 +91,9 @@ function length(string) {
    */
   function endsWith(string, char) {
     // YOUR CODE BELOW HERE //
-  
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given two input Strings, return the Strings concatenated into one.
    *
@@ -105,10 +101,9 @@ function length(string) {
    */
   function concat(stringOne, stringTwo) {
     // YOUR CODE BELOW HERE //
-  
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given any number of Strings, return all of them joined together.
    *
@@ -122,10 +117,10 @@ function length(string) {
   function join(stringOne, stringTwo) {
     // YOUR CODE BELOW HERE //
     var args = Array.from(arguments);
-  
+
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given two Strings, return the longest of the two.
    *
@@ -137,10 +132,9 @@ function length(string) {
    */
   function longest(stringOne, stringTwo) {
     // YOUR CODE BELOW HERE //
-  
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given two Strings, return 1 if the first string is higher in alphabetical order (meaning earlier in the alphabet) than
    * the second, return -1 if the second is higher in alphabetical order than the
@@ -150,10 +144,9 @@ function length(string) {
    */
   function sortAscending(stringOne, stringTwo) {
     // YOUR CODE BELOW HERE //
-  
     // YOUR CODE ABOVE HERE //
   }
-  
+
   /**
    * Given two Strings, return 1 if the first is lower in alphabetical order (meaning later in the alphabet) than
    * the second, return -1 if the second is lower in alphabetical order than the
@@ -163,15 +156,14 @@ function length(string) {
    */
   function sortDescending(stringOne, stringTwo) {
     // YOUR CODE BELOW HERE //
-    
+    //return the sortDescending function
+    return (stringOne, stringTwo);
     // YOUR CODE ABOVE HERE //
   }
-  
+
   // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-  if (
-    typeof process !== 'undefined' &&
-    typeof process.versions.node !== 'undefined'
-  ) {
+  if (typeof process !== 'undefined' &&
+    typeof process.versions.node !== 'undefined') {
     // here, export any references you need for tests //
     module.exports.length = length;
     module.exports.toLowerCase = toLowerCase;
@@ -185,15 +177,4 @@ function length(string) {
     module.exports.sortAscending = sortAscending;
     module.exports.sortDescending = sortDescending;
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+}
